140d1cbae1561a6f7eae9e70fcb3a89f8e0a9559
Abhijeet Panda <abhijeetpanda06@gmail.com>
2024-01-19 10:08:26 +0530

    added sign in page and functionality

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -61,7 +61,7 @@ const AuthProvider = ({children}:{ children : React.ReactNode}) => {
     useEffect(()=>{
         if(
             localStorage.getItem('cookieFallback')=== '[]'
-            //||localStorage.getItem('cookieFallback') === null
+            ||localStorage.getItem('cookieFallback') === null
         ) navigate('/sign-in')
 
         checkAuthUser();
