95391c4ca642ff6dc7db9b743567471531a45485
Abhijeet Panda <abhijeetpanda06@gmail.com>
2024-02-01 09:01:01 +0530

    added topbar tp home

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -4,7 +4,7 @@ import {
     useQueryClient,
     useInfiniteQuery
  } from "@tanstack/react-query";
-import { createUserAccount, signInAccount } from "../appwrite/api";
+import { createUserAccount, signInAccount, signOutAccount } from "../appwrite/api";
 import { INewUser } from "@/types";
 
 
@@ -20,5 +20,11 @@ import { INewUser } from "@/types";
             email : string;
             password : string
         }) => signInAccount(user)
+    });
+ }
+
+ export const useSignOutAccount = () =>{
+    return useMutation({
+        mutationFn : signOutAccount
     });
  }
